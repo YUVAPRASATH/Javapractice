859af8f81124047fa88e9916a871ff8dacab3d8a
Yuvaprasath N <yuvaprasath.nallathambi@sgdigital.com>
2020-05-04 01:31:46 +0530

    Stream and System

---
+++ src/com/javapractice/stream/Stream.java
@@ -0,0 +1,34 @@
+package com.javapractice.stream;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+public class Stream {
+    public static void main(String args[]){
+        // Data
+        //List<Integer> arrayList = Arrays.asList(1,5,7,9,null);
+        List<Integer> arrayList = new ArrayList<>();
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(null);
+        arrayList.add(1);
+        arrayList.add(2);
+
+        System.out.println(arrayList);
+        System.out.println(arrayList.stream().anyMatch(integer -> {
+            if(integer==null)
+                 return true;
+            return false;
+        } ) );
+        List distinctList = Arrays.asList(arrayList.stream().distinct().toArray());
+        System.out.println( distinctList);
+      // arrayList.stream().
+       // TODO comparator, predicate, supplier, conusmer
+        arrayList.add(1);
+        System.out.println(arrayList);
+
+        // TODO parallel stream
+    }
+}
